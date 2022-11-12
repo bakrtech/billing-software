964f35fa3f13e980a33994a54a8db7b6683a1ebc
BakrTech <abubakrniazi786@gmail.com>
2022-11-12 15:57:59 +0000

    hmm

---
+++ InformationHandler.py
@@ -0,0 +1,17 @@
+import pickle
+def Inofrmation():
+    Info = open("information.dat","ab")
+    Store_Name = str(input("Enter the store ID :"))
+    Store_Mobile_No=str(input("Enter the Store Moblie number: "))
+    Store_Address=input('Enter the Store Address: ')
+    Store_GSTID=input('Enter GST invoice id: ')
+    Store_Disclaimer=input('Enter the Disclaimer for the store :')
+    pickle.dump([Store_Name,Store_Mobile_No,Store_Address,Store_GSTID,Store_Disclaimer],Info)
+    Info.close()
+Inofrmation()
+'''
+1) Billing page  
+2) Stonks
+3) exit
+4) 
+'''
--- main.py
+++
@@ -1,3 +0,0 @@
-Info = open("information.dat","a")
-
-Info.close()
