788985a13bc82ddf5d86b1a3b8991899e93655ee
BakrTech <abubakrniazi786@gmail.com>
2022-11-12 16:09:50 +0000

    BILLING

---
+++ biling.py
@@ -0,0 +1,22 @@
+'''
+append item 
+remove ''
+calc
+product info
+
+
+>CODE 
+display infor mationa
+.
+.
+.
+.
+.
+.
+Number of titma n stocks =21
+Quantiy > 2
+ADDING {quantity} * XYZ in bill
+CONFIRM >y/n
+add
+
+'''
